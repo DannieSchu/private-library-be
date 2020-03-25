f8c6a4cf574ae26a92550b0f4c67f9c0f475bddc
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-25 16:01:00 -0700

    write failing test to get author by id

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -1,10 +1,25 @@
-const { getAuthor } = require('../db/data-helpers');
+const { getAuthor, getBooks } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-const Author = require('../lib/models/Author');
+// const connect = require('../lib/utils/connect');
+// const mongoose = require('mongoose');
+// const Author = require('../lib/models/Author');
+
+/*
+[x]`POST /api/v1/authors` to create a new author
+[]`GET /api/v1/authors` to get all authors
+[]`GET /api/v1/authors/:id` to get an author by ID
+[]`PATCH /api/v1/authors/:id` to update an author
+[]`DELETE /api/v1/authors/:id` to delete an author
+
+[]`POST /api/v1/books` create a new book
+[]`GET /api/v1/books/:id` get a book by id and populate tweet
+[]`PATCH /api/v1/books/:id` update a book
+[]`DELETE /api/v1/books/:id` delete a book
+*/
+
+
 
 describe('author routes', () => {
   it('creates an author', () => {
@@ -18,6 +33,23 @@ describe('author routes', () => {
           _id: expect.any(String),
           name: 'Fyodor Dostoyevski',
           __v: 0
+        });
+      });
+  });
+
+  it('gets an author', async() => {
+    const author = await getAuthor();
+    const books = await getBooks();
+
+    return request(app)
+      .post(`/api/v1/${author._id}`)
+      .send({
+        name: 'Fyodor Dostoyevski'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...author,
+          books        
         });
       });
   });
