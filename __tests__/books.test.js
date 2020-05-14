e8edcc790ab33fb1ed8c9b44f4fe6e9479a41ca2
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-13 17:01:29 -0700

    populate comments in route for getting all books

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,4 +1,4 @@
-const { getAuthor, getBook, getBooks } = require('../db/data-helpers');
+const { getAuthor, getBook, getBooks, getComments } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -40,12 +40,14 @@ describe('book routes', () => {
   it('gets a book by its id', async() => {
     const author = await getAuthor();
     const book = await getBook({ authorId: author._id });
+    const comments = await getComments({ book: book._id });
 
     return request(app)
       .get(`/api/v1/books/${book._id}`)
       .then(res => {
         expect(res.body).toEqual({
           ...book,
+          comments,
           authorId: author
         });
       });
